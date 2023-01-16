7c36897b47bbdb97075cea6efa02ece645b3c99b
Vaishnavi Paliya <vaishnavipaliya11@gmail.com>
2023-01-16 15:19:15 +0530

    fix: deploy fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,17 +2,15 @@ import { Sidebar } from "../components/sidebar/Sidebar";
 import "../styles.css";
 import { data } from "../data/stateData";
 import { DisplayCards } from "../components/displayCards/DisplayCards";
-import { useState } from "react";
 import { Navbar } from "../components/navbar/Navbar";
 export const Home = () => {
-  const [cardData, setCardData] = useState(data);
   console.log(data);
   return (
     <div className="main-container">
       <Sidebar />
       <div>
-        <Navbar/>
-       
+        <Navbar />
+
         <div className="common-flex-row  center page-bg bet">
           <div className="common-flex-row margin-l">
             <p>ALL</p>
@@ -22,11 +20,11 @@ export const Home = () => {
           </div>
 
           <label>
-            <input type="search" placeholder="Select team" id="team-input"/>
+            <input type="search" placeholder="Select team" id="team-input" />
           </label>
         </div>
         <div class="common-flex-row wrap products-mapped page-bg">
-          {cardData.map((item) => {
+          {data.map((item) => {
             return <DisplayCards item={item} />;
           })}
         </div>

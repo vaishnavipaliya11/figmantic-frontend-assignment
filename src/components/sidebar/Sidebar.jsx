e5a5ae48474eecacd3d4f813f14e8da699e2edd3
Vaishnavi Paliya <vaishnavipaliya11@gmail.com>
2023-01-16 13:48:47 +0530

    style: sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,46 +1,76 @@
 import "../../styles.css";
+import { MdDashboard } from "react-icons/md";
+import { SiKnowledgebase } from "react-icons/si";
+import { SiMicrosoftteams } from "react-icons/si";
+import { BsChatLeftQuote } from "react-icons/bs";
+import { FiSettings } from "react-icons/fi";
+import { MdBusiness } from "react-icons/md";
+import { FaUserCircle } from "react-icons/fa";
 export const Sidebar = () => {
   return (
     <div className="common-flex-col sidebar-container start">
       <div className="common-flex-col start">
-        <div>
+        <div className="common-flex-row center">
           {" "}
-          <p> dashboard</p>
+          <p>
+            <MdDashboard />
+          </p>
+          <p className="margin-sm"> Dashboard</p>
         </div>
-        <div>
+        <div className="common-flex-row center">
           {" "}
-          <p> Knowledge</p>
+          <p>
+            {" "}
+            <SiKnowledgebase />{" "}
+          </p>
+          <p className="margin-sm"> Knowledge</p>
         </div>
-        <div>
+        <div className="common-flex-row center">
           {" "}
-          <p> Members</p>
+          <p>
+            <SiMicrosoftteams />
+          </p>
+          <p className="margin-sm"> Members</p>
         </div>
-        <div>
+        <div className="common-flex-row center">
           {" "}
-          <p> teams</p>
+          <p>
+            <SiMicrosoftteams />
+          </p>
+          <p className="margin-sm"> Teams</p>
         </div>
-        <div>
+        
+        <div className="common-flex-row center">
           {" "}
-          <p> vault</p>
-        </div>
-        <div>
-          {" "}
-          <p> chat</p>
+          <p>
+            <BsChatLeftQuote />
+          </p>
+          <p className="margin-sm"> Chat</p>
         </div>
       </div>
 
       <div className="common-flex-col start">
-        <div>
+        <div className="common-flex-row center">
           {" "}
-          <p> Setting</p>
+          <p>
+            <FiSettings />
+          </p>
+          <p className="margin-sm"> Setting</p>
         </div>
-        <div>
+        <div className="common-flex-row center">
           {" "}
-          <p> company</p>
+          <p>
+            <MdBusiness />{" "}
+          </p>
+          <p className="margin-sm"> Company</p>
         </div>
-        <div>
+        <div className="common-flex-row center">
           {" "}
-          <p> my-profile</p>
+          <p>
+            {" "}
+            <FaUserCircle />{" "}
+          </p>
+          <p className="margin-sm"> My-Profile</p>
         </div>
       </div>
     </div>
